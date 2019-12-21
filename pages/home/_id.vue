ecd3de723bba25663ea9148a11309999e951465d
snowball-win <1091457391@qq.com>
2019-12-21 21:25:36 +0800

    动态路由/_id

---
+++ pages/home/_id.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="container">
+    <div>
+      <!-- <logo /> -->
+    </div>
+  </div>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+export default {
+  components: {
+    Logo
+  },
+  data(){
+    return {
+      name: 'yiyi'
+    }
+  },
+  computed:{
+  },
+  methods:{
+  },
+  mounted(){
+    console.log('st',this._routerRoot._route.params.id)
+  }
+}
+</script>
+
+<style>
+</style>
+// 在Nuxt.js里面定义带参数的动态路由，需要创建对应的以下划线作为前缀的Vue文件或目录
